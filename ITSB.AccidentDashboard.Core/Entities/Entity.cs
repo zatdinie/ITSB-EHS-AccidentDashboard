a904e5c73c264aefe369176f8c26aab08a580416
Your Name <your.email@example.com>
2026-09-10 15:02:38 +0800

    initial commit

---
+++ ITSB.AccidentDashboard.Core/Entities/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITSB.AccidentDashboard.Core.Entities
+{
+    internal class Entity
+    {
+    }
+}
---
+++ ITSB.AccidentDashboard/README.md
@@ -0,0 +1,1 @@
+# ITSB.AccidentDashboard
