19824410605f17230000345e9b1b07a80e90c7ec
Your Name <your.email@example.com>
2026-09-11 10:05:38 +0800

    core entities, EF6 DbContext, initial migrations

---
+++ ITSB.AccidentDashboard.Core/EshDbCotext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+public class EshDbContext : DbContext
+{
+    public EshDbContext() : base("name=EshDb"){}
+
+    public DbSet<Plant> Plants { get; set; }
+    public DbSet<AccidentCause> AccidentCauses { get; set; }
+    public DbSet<BodyPart> BodyParts { get; set; }
+    public DbSet<InjuryType> InjuryTypes { get; set; }
+    public DbSet<AccidentIncident> AccidentIncidents { get; set; }
+    public DbSet<MonthlyManHours> MonthlyManHours { get; set; }
+}
---
+++ ITSB.AccidentDashboard.Core/Repositories/PlantRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+
+public class PlantRepository
+{
+    public List<Plant> GetAll()
+    {
+        using (var db = new EshDbContext())
+            return db.Plants.OrderBy(p => p.Code).ToList();
+    }
+    
+    public Plant GetById(int id)
+    {
+        using (var db = new EshDbContext())
+            return db.Plants.Find(id);
+    }
+
+    public void Update(Plant plant)
+    {
+        using (var db = new EshDbContext())
+        {
+            db.Entry(plant).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+
+    public void Delete(int id)
+    {
+        using (var db = new EshDbContext())
+        {
+            var plant = db.Plants.Find(id);
+            if (plant != null)
+            {
+                db.Plants.Remove(plant);
+                db.SaveChanges();
+            }
+        }
+    }
+}
