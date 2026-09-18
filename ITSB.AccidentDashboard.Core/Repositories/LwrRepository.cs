2c2de147d6cf526345293723766d0afe94444689
Your Name <your.email@example.com>
2026-09-18 09:31:07 +0800

    Rebuild LWR page with full rate table and Rebuild Accident summary table

--- ITSB.AccidentDashboard.Core/Repositories/LwrRepository.cs
+++ ITSB.AccidentDashboard.Core/Repositories/LwrRepository.cs
@@ -1,17 +1,26 @@
 ﻿using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
+using ITSB.AccidentDashboard.Core.Entities;
 
 namespace ITSB.AccidentDashboard.Core.Repositories
 {
     public class MonthlyRate
     {
         public int Month { get; set; }
+        public int TotalAccidents { get; set; }        // A
+        public int LostWorkDayCases { get; set; }       // B
+        public int LostWorkdaysCount { get; set; }       // C
+        public double SeverityRate { get; set; }         // D
+        public double? TargetSeverityRate { get; set; }  // E (prior year * 0.9, null if no prior data)
+        public double Lwdi { get; set; }                 // F
+        public double Tcir { get; set; }                 // G
+
+        public int DLHeadcount { get; set; }
+        public int DLHours { get; set; }
+        public int IDLHeadcount { get; set; }
+        public int IDLHours { get; set; }
         public int TotalHours { get; set; }
-        public int LostWorkDayCases { get; set; }
-        public int RecordableCases { get; set; }
-        public double Lwdi { get; set; }
-        public double Tcir { get; set; }
+        public int TotalHeadcount { get; set; }
     }
 
     public class LwrRepository
@@ -22,33 +31,56 @@ namespace ITSB.AccidentDashboard.Core.Repositories
         {
             using (var db = new EshDbContext())
             {
-                var incidents = db.AccidentIncidents
-                    .Where(i => i.DateOfOccurrence.Year == year)
-                    .ToList();
-                var hours = db.MonthlyManHours
-                    .Where(m => m.Year == year)
-                    .ToList();
+                var current = ComputeRatesForYear(db, year);
+                var prior = ComputeRatesForYear(db, year - 1);
 
-                var result = new List<MonthlyRate>();
-                for (int m = 1; m <= 12; m++)
+                for (int i = 0; i < current.Count; i++)
                 {
-                    var monthHours = hours.Where(h => h.Month == m)
-                        .Sum(h => h.DirectLaborHours + h.IndirectLaborHours);
-                    var lwdCases = incidents.Count(i => i.DateOfOccurrence.Month == m && i.IsLostWorkDayCase);
-                    var recCases = incidents.Count(i => i.DateOfOccurrence.Month == m && i.IsRecordableCase);
-
-                    result.Add(new MonthlyRate
-                    {
-                        Month = m,
-                        TotalHours = monthHours,
-                        LostWorkDayCases = lwdCases,
-                        RecordableCases = recCases,
-                        Lwdi = monthHours > 0 ? (double)lwdCases * OshaBase / monthHours : 0,
-                        Tcir = monthHours > 0 ? (double)recCases * OshaBase / monthHours : 0
-                    });
+                    current[i].TargetSeverityRate = prior[i].TotalHours > 0
+                        ? (double?)(prior[i].SeverityRate * 0.9)
+                        : null;
                 }
-                return result;
+                return current;
             }
+        }
+
+        private List<MonthlyRate> ComputeRatesForYear(EshDbContext db, int year)
+        {
+            var incidents = db.AccidentIncidents.Where(i => i.DateOfOccurrence.Year == year).ToList();
+            var hours = db.MonthlyManHours.Where(h => h.Year == year).ToList();
+
+            var result = new List<MonthlyRate>();
+            for (int m = 1; m <= 12; m++)
+            {
+                var monthHours = hours.Where(h => h.Month == m).ToList();
+                int dlHc = monthHours.Sum(h => h.DirectLaborHeadcount);
+                int dlHrs = monthHours.Sum(h => h.DirectLaborHours);
+                int idlHc = monthHours.Sum(h => h.IndirectLaborHeadcount);
+                int idlHrs = monthHours.Sum(h => h.IndirectLaborHours);
+                int totalHours = dlHrs + idlHrs;
+
+                int totalAccidents = incidents.Count(i => i.DateOfOccurrence.Month == m);
+                int lwdCases = incidents.Count(i => i.DateOfOccurrence.Month == m && i.IsLostWorkDayCase);
+                int lostWorkdays = incidents.Where(i => i.DateOfOccurrence.Month == m).Sum(i => i.LostWorkDays);
+
+                result.Add(new MonthlyRate
+                {
+                    Month = m,
+                    TotalAccidents = totalAccidents,
+                    LostWorkDayCases = lwdCases,
+                    LostWorkdaysCount = lostWorkdays,
+                    SeverityRate = totalHours > 0 ? (double)lostWorkdays * OshaBase / totalHours : 0,
+                    Lwdi = totalHours > 0 ? (double)lwdCases * OshaBase / totalHours : 0,
+                    Tcir = totalHours > 0 ? (double)totalAccidents * OshaBase / totalHours : 0,
+                    DLHeadcount = dlHc,
+                    DLHours = dlHrs,
+                    IDLHeadcount = idlHc,
+                    IDLHours = idlHrs,
+                    TotalHours = totalHours,
+                    TotalHeadcount = dlHc + idlHc
+                });
+            }
+            return result;
         }
     }
 }
